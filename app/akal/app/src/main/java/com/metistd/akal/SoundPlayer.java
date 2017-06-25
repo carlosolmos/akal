d6817b6ce4b116a0a2b8297d062362ccb8d9028e
Carlos Olmos <carlos@intelesense.net>
2017-06-24 17:46:57 -0700

    Timer countdown and 1 pre-set chime sound.

--- app/akal/app/src/main/java/com/metistd/akal/SoundPlayer.java
+++ app/akal/app/src/main/java/com/metistd/akal/SoundPlayer.java
@@ -1,10 +1,15 @@
 package com.metistd.akal;
 
 import android.content.Context;
+import android.media.AudioAttributes;
 import android.media.AudioManager;
 import android.media.SoundPool;
+import android.os.Build;
+import android.util.Log;
 
 import java.util.HashMap;
+
+import static android.media.AudioManager.PROPERTY_OUTPUT_SAMPLE_RATE;
 
 /**
  * Created by carlosolmos on 6/24/17.
@@ -17,12 +22,33 @@ public class SoundPlayer {
 
 
     public static void initSounds(Context context) {
-        soundPool = new SoundPool.Builder()
-                .setMaxStreams(10)
+        int sampleRate = getBestSampleRate(context);
+
+        AudioAttributes attributes = new AudioAttributes.Builder()
+                .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                .setFlags(AudioAttributes.FLAG_AUDIBILITY_ENFORCED)
+                .setUsage(AudioAttributes.USAGE_ALARM)
+                .build();
+
+        soundPool = new SoundPool.Builder().setAudioAttributes(attributes)
+                .setMaxStreams(2)
                 .build();
         soundPoolMap = new HashMap<Integer, Integer>();
         soundPoolMap.put( CHIME1, soundPool.load(context, R.raw.chime1, 1) );
 
+    }
+    public static int getBestSampleRate(Context context)
+    {
+        if (Build.VERSION.SDK_INT >= 17) {
+            AudioManager am = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+            String sampleRateString = am.getProperty(AudioManager.PROPERTY_OUTPUT_SAMPLE_RATE);
+            Log.d("SoundPlayer", "Sample Rate: " + sampleRateString);
+            int sampleRate = sampleRateString == null ? 44100 : Integer.parseInt(sampleRateString);
+
+            return sampleRate;
+        } else {
+            return 44100;
+        }
     }
 
     public static void playSound(Context context, int soundID) {
--- app/akal/app/src/main/java/com/metistd/akal/TimerFragment.java
+++ app/akal/app/src/main/java/com/metistd/akal/TimerFragment.java
@@ -1,14 +1,22 @@
 package com.metistd.akal;
 
 import android.content.Context;
+import android.media.AudioManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
 
 
 /**
@@ -24,12 +32,41 @@ public class TimerFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
+    public static final String UNIT_MINUTES = "minutes";
+    public static final String UNIT_SECONDS = "seconds";
+
     private SoundPlayer soundPlayer;
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
     private OnFragmentInteractionListener mListener;
+
+    private LinearLayout iconlayout;
+    private LinearLayout clockLayout;
+    private LinearLayout clockPickerLayout;
+
+    private ImageView timerIcon;
+
+    //clock elements.
+    private TextView txtClockPlayPause;
+    private TextView txtClockStop;
+    private boolean isTimerOn = false;
+    private boolean isTimerRunning = false;
+    private int currentMinutes = 0;
+    private int currentSeconds = 0;
+    private TextView txtTimerDisplay;
+    private CountDownTimer countDownTimer;
+    private long remainingMilliseconds = 0;
+
+    //clock picker
+    private TextView txtPickerAccept;
+    private TextView txtPickerCancel;
+    private HashMap<Integer, Integer> validMinutesPicks;
+    private HashMap<Integer, Integer> validSecondsPicks;
+    private HashMap<Integer, TextView> mapPickerMinutes;
+    private HashMap<Integer, TextView> mapPickerSeconds;
 
     public TimerFragment() {
         // Required empty public constructor
@@ -69,27 +106,231 @@ public class TimerFragment extends Fragment {
                              Bundle savedInstanceState) {
         View timerView = inflater.inflate(R.layout.fragment_timer, container, false);
 
-        ImageView timerIcon = (ImageView) timerView.findViewById(R.id.imgTimerIcon);
-        timerIcon.setOnClickListener(new View.OnClickListener()
-        {
+        //timer icon/button
+        iconlayout = (LinearLayout) timerView.findViewById(R.id.iconlayout);
+        iconlayout.setVisibility(View.VISIBLE);
+        timerIcon = (ImageView) timerView.findViewById(R.id.imgTimerIcon);
+        timerIcon.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view)
-            {
-                Log.d("Test", "onClickListener ist gestartet");
-                soundPlayer.playSound(getContext(), SoundPlayer.CHIME1);
+            public void onClick(View view) {
+                Log.d("TimerFragment", "Activating timer controls");
+                //soundPlayer.playSound(getContext(), SoundPlayer.CHIME1);
+                showClockPicker();
             }
         });
-        // Inflate the layout for this fragment
+
+        //clock picker.
+        clockPickerLayout = (LinearLayout) timerView.findViewById(R.id.clockPickerLayout);
+        clockPickerLayout.setVisibility(View.INVISIBLE);
+        txtPickerCancel = (TextView) timerView.findViewById(R.id.txtClockPickerCancel);
+        txtPickerCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("TimerFragment", "Cancel picker controls");
+                cancelPicker(view);
+            }
+        });
+
+        txtPickerAccept = (TextView) timerView.findViewById(R.id.txtClockPickerAccept);
+        txtPickerAccept.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("TimerFragment", "Accept new timer pick");
+                acceptPicker(view);
+            }
+        });
+
+        //clock display.
+        clockLayout = (LinearLayout) timerView.findViewById(R.id.clocklayout);
+        clockLayout.setVisibility(View.INVISIBLE);
+        txtTimerDisplay = (TextView) timerView.findViewById(R.id.txtClockDisplay);
+        txtClockPlayPause = (TextView) timerView.findViewById(R.id.txtClockPlayPause);
+        txtClockStop = (TextView) timerView.findViewById(R.id.txtClockStop);
+        txtClockStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stopTimer(view);
+            }
+        });
+        txtClockPlayPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startTimer(view);
+            }
+        });
+        validMinutesPicks = new HashMap<Integer, Integer>();
+        validMinutesPicks.put(R.id.txtClockControlMinute0, 0);
+        validMinutesPicks.put(R.id.txtClockControlMinute1, 1);
+        validMinutesPicks.put(R.id.txtClockControlMinute3, 3);
+        validMinutesPicks.put(R.id.txtClockControlMinute5, 5);
+        validMinutesPicks.put(R.id.txtClockControlMinute10, 10);
+        validMinutesPicks.put(R.id.txtClockControlMinute15, 15);
+        mapPickerMinutes = new HashMap<Integer, TextView>();
+        for (int pick : validMinutesPicks.keySet()) {
+            TextView txtPickerMinutes = (TextView) timerView.findViewById(pick);
+            mapPickerMinutes.put(pick, txtPickerMinutes);
+            final int item = pick;
+            ((TextView)mapPickerMinutes.get(pick)).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    setClockPickerValue(view, UNIT_MINUTES, validMinutesPicks.get(item));
+                }
+            });
+        }
+
+        validSecondsPicks = new HashMap<Integer, Integer>();
+        validSecondsPicks.put(R.id.txtClockControlSecond0, 0);
+        validSecondsPicks.put(R.id.txtClockControlSecond10, 10);
+        validSecondsPicks.put(R.id.txtClockControlSecond20, 20);
+        validSecondsPicks.put(R.id.txtClockControlSecond30, 30);
+        mapPickerSeconds = new HashMap<Integer, TextView>();
+        for (int pick : validSecondsPicks.keySet()) {
+            TextView txtPickerSeconds = (TextView) timerView.findViewById(pick);
+            mapPickerSeconds.put(pick, txtPickerSeconds);
+            final int item = pick;
+            ((TextView)mapPickerSeconds.get(pick)).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    setClockPickerValue(view, UNIT_SECONDS, validSecondsPicks.get(item));
+                }
+            });
+        }
+
         //return
         return timerView;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
+    public void showTimerControls() {
+        iconlayout.setVisibility(View.INVISIBLE);
+        clockPickerLayout.setVisibility(View.INVISIBLE);
+        clockLayout.setVisibility(View.VISIBLE);
+    }
+
+    public void showClockPicker() {
+        iconlayout.setVisibility(View.INVISIBLE);
+        clockLayout.setVisibility(View.INVISIBLE);
+        clockPickerLayout.setVisibility(View.VISIBLE);
+        resetTimerControls();
+
+    }
+
+    public void resetTimerControls() {
+        txtTimerDisplay.setText("00:00");
+        txtTimerDisplay.setTextColor(ContextCompat.getColor(getContext(), R.color.colorPurple));
+        txtClockPlayPause.setText("Start");
+        txtClockPlayPause.setTextColor(ContextCompat.getColor(getContext(), R.color.colorWhite));
+        txtClockStop.setTextColor(ContextCompat.getColor(getContext(), R.color.colorWhite));
+        allPickersWhite(mapPickerMinutes);
+        allPickersWhite(mapPickerSeconds);
+    }
+
+    public void closeTimerContols() {
+        iconlayout.setVisibility(View.VISIBLE);
+        clockLayout.setVisibility(View.INVISIBLE);
+        clockPickerLayout.setVisibility(View.INVISIBLE);
+        resetTimerControls();
+    }
+
+    public void stopTimer(View view) {
+        Log.d("TimerFragment", "Stopping timer");
+        isTimerOn = false;
+        isTimerRunning = false;
+        currentMinutes = 0;
+        currentSeconds = 0;
+        resetTimerControls();
+        Log.d("TimerFragment", "Closing timer controls");
+        closeTimerContols();
+    }
+
+    public void startTimer(View view){
+        if(isTimerOn){
+            if(isTimerRunning){
+                //pause
+                countDownTimer.cancel();
+                txtClockPlayPause.setText("Start");
+                isTimerRunning = false;
+            }else{
+                //resume.
+                initCountDown(remainingMilliseconds);
+                txtClockPlayPause.setText("Pause");
+                isTimerRunning = true;
+            }
+        }else{
+            //start from scratch.
+            Log.d("TimerFragment", "Starting timer. Min: " + currentMinutes + " Secs: " + currentSeconds);
+            isTimerOn = true;
+            txtClockPlayPause.setText("Pause");
+            txtClockPlayPause.setTextColor(ContextCompat.getColor(getContext(), R.color.colorWhite));
+            long runForMillis = ((currentMinutes * 60)  + currentSeconds) * 1000;
+            initCountDown(runForMillis);
+            countDownTimer.start();
+            isTimerRunning = true;
         }
     }
+
+    public void initCountDown(long milliseconds){
+        countDownTimer = new CountDownTimer(milliseconds, 1000) {
+            public void onTick(long millisUntilFinished) {
+                remainingMilliseconds = millisUntilFinished;
+                String timerString = String.format("%02d:%02d",
+                        TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished),
+                        TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) -
+                                TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished))
+                );
+                txtTimerDisplay.setText(timerString);
+            }
+
+            public void onFinish() {
+                txtTimerDisplay.setText("00:00");
+                txtClockPlayPause.setText("Repeat");
+                isTimerRunning = false;
+                isTimerOn = false;
+                chime();
+            }
+        };
+    }
+    public void chime(){
+        soundPlayer.playSound(getContext(), SoundPlayer.CHIME1);
+    }
+    public void acceptPicker(View view) {
+        if (currentSeconds > 0 || currentMinutes > 0) {
+            Log.d("TimerFragment", "Current Timer Pick Min: " + currentMinutes + " Secs: " + currentSeconds);
+            String timerString = String.format("%02d:%02d", currentMinutes,currentSeconds);
+            txtTimerDisplay.setText(timerString);
+            showTimerControls();
+        }
+    }
+
+    public void cancelPicker(View view) {
+        currentMinutes = 0;
+        currentSeconds = 0;
+        closeTimerContols();
+    }
+
+    public void setClockPickerValue(View view, String unit, int value) {
+        switch (unit) {
+            case UNIT_MINUTES:
+                currentMinutes = value;
+                allPickersWhite(mapPickerMinutes);
+                ((TextView)view).setTextColor(ContextCompat.getColor(getContext(), R.color.colorPurple));
+                break;
+            case UNIT_SECONDS:
+                currentSeconds = value;
+                allPickersWhite(mapPickerSeconds);
+                ((TextView)view).setTextColor(ContextCompat.getColor(getContext(), R.color.colorPurple));
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    public void allPickersWhite(HashMap<Integer, TextView> mapOfElements){
+        for(int key : mapOfElements.keySet()){
+            mapOfElements.get(key).setTextColor(ContextCompat.getColor(getContext(), R.color.colorWhite));
+        }
+    }
+
 
     /*@Override
     public void onAttach(Context context) {
@@ -121,5 +362,12 @@ public class TimerFragment extends Fragment {
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
     }
 }
