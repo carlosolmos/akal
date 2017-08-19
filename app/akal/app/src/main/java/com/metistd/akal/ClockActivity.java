186932f3e3dd41f9aa0fe5920ca7b4e35b0476cd
Carlos Olmos <carlos@intelesense.net>
2017-08-19 15:01:39 -0700

    aka.

---
+++ app/akal/app/src/main/java/com/metistd/akal/ClockActivity.java
@@ -0,0 +1,14 @@
+package com.metistd.akal;
+
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ClockActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clock);
+    }
+}
